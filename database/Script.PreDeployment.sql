d7b0b21212ece45d8dc875a1e78e78dd1e687b10
Muriel Thevenaz <m.thevenaz@gmail.com>
2023-04-03 16:43:55 +0200

    predeployment script removed

--- database/Script.PreDeployment.sql
+++
@@ -1,8 +0,0 @@
-if (exists(select * from sys.columns where object_id = object_id('dbo.medications')))
-begin
-    truncate table dbo.medications;
-end
-if (exists(select * from sys.columns where object_id = object_id('dbo.categories'))
-begin
-    truncate table dbo.categories;
-end
