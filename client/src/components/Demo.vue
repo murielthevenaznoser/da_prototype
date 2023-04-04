27c3ae04c140a9472db963403a981781ebfaa9e0
Muriel Thevenaz <m.thevenaz@gmail.com>
2023-04-04 05:29:53 +0200

    testing getting meds

--- client/src/components/Demo.vue
+++ client/src/components/Demo.vue
@@ -3,19 +3,17 @@
     <header class="header">
       <h1>DEMO</h1>
       <h2 v-if="isLoading">Loading...</h2>     
-      <input class="check-medication" autofocus autocomplete="off" placeholder="Quel médicament doit être recherché ?" v-model="medication"
+      <input class="check-medication" autofocus autocomplete="off" placeholder="Quel médicament doit être recherché ?" v-model="searchInput"
         @keyup.enter="searchMedication" />
     </header>
-    <section class="main" v-show="response.length">
-      <ul class="response-list">        
-        <li v-for="response in response-list" class="response" :key="response.id"></li>
-      </ul>
-    </section>
+    <footer class="footer">Copyright by ALLIANs allergY SaRL</footer>
   </section>
 </template>
 
+
 <script lang="js">
-const API = "/data-api/rest/medications";
+const MEDICATIONS = "/data-api/rest/medications";
+const CATEGORIES = "/data-api/rest/categories";
 const HEADERS = { 'Accept': 'application/json', 'Content-Type': 'application/json;charset=utf-8' };
 
 
@@ -23,38 +21,31 @@ export default {
 
   data() {
     return {
+      searchInput: '',
       response: [],
       isLoading: false
     };
   },
 
-  mounted() {    
-    this.getMedications();
-  },
-
-  watch: {
-    isLoading(newValue) {
-      if (newValue == true) {
-        this.$Progress.start();    
-      }       
-      if (newValue == false) {
-        this.$Progress.finish();
-      } 
-    }
-  },
-
   methods: {
-    getMedications: function () {
+    searchMedication: function () {
       this.isLoading = true;
-      
-      fetch(API, { headers: HEADERS, method: "GET" })
-        .then(res => { return res.json(); })
-        .then(res => {
-          this.response = res == null ? [] : res.value;                                            
-          this.isLoading = false;          
-        }, res => {
-          this.isLoading = false;
-        });
+
+      var value = this.searchInput && this.searchInput.trim();
+      if (!value) return;
+      fetch(MEDICATIONS, {
+        headers: HEADERS,
+        method: "GET"})
+      .then(res => {
+        console.log('response', res);
+        if (res.ok) {
+          this.searchInput = ''
+          return res.json();
+        }
+      }).then(res => {
+        this.response = res.value;
+        this.isLoading = false;
+      })
     }
   },
 };
